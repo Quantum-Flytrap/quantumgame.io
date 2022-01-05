db06ecdc5688c24eae08ca03117c73586e782915
Frizi <frizi09@gmail.com>
2022-01-05 12:02:15 +0100

    deregister old service worker

---
+++ static/service-worker.js
@@ -0,0 +1,17 @@
+// Self-destroying service worker.
+// For any client that requests an old service worker file,
+// this will instruct it to forget about it and reload.
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
+});
